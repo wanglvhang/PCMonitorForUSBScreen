11326bddf28ae17b1581349b6e810ef3eaac4db7
wanglvhang <wanglvhang@qq.com>
2021-08-10 11:51:12 +0800

    Update Main.Designer.cs

--- PCMonitor.UI/Main.Designer.cs
+++ PCMonitor.UI/Main.Designer.cs
@@ -393,7 +393,7 @@ namespace PCMonitor.UI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(535, 458);
+            this.ClientSize = new System.Drawing.Size(535, 465);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.ckbScreenProtect);
@@ -402,6 +402,7 @@ namespace PCMonitor.UI
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.btnStop);
             this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Main";
